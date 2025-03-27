106ba9b307f07e1ea9661c94d6a271aae8c6cbfd
Vincenzo Manica <vincenzomanica@outlook.com>
2025-03-27 11:02:03 -0300

    ffff

--- Projeto_Imoveis/frmCapturaImagem.Designer.cs
+++ Projeto_Imoveis/frmCapturaImagem.Designer.cs
@@ -23,31 +23,34 @@
             // pctBoxCaptura
             // 
             this.pctBoxCaptura.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.pctBoxCaptura.Location = new System.Drawing.Point(12, 12);
+            this.pctBoxCaptura.Location = new System.Drawing.Point(16, 15);
+            this.pctBoxCaptura.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pctBoxCaptura.Name = "pctBoxCaptura";
-            this.pctBoxCaptura.Size = new System.Drawing.Size(640, 404);
+            this.pctBoxCaptura.Size = new System.Drawing.Size(853, 497);
             this.pctBoxCaptura.TabIndex = 0;
             this.pctBoxCaptura.TabStop = false;
             // 
             // btnCapturar
             // 
-            this.btnCapturar.Font = new System.Drawing.Font("Modern No. 20", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCapturar.Location = new System.Drawing.Point(577, 422);
+            this.btnCapturar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapturar.Location = new System.Drawing.Point(769, 519);
+            this.btnCapturar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCapturar.Name = "btnCapturar";
-            this.btnCapturar.Size = new System.Drawing.Size(75, 43);
+            this.btnCapturar.Size = new System.Drawing.Size(100, 53);
             this.btnCapturar.TabIndex = 1;
-            this.btnCapturar.Text = "Capturar";
+            this.btnCapturar.Text = "Capturar 1";
             this.btnCapturar.UseVisualStyleBackColor = true;
             this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
             // 
             // frmCapturaImagem
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(664, 477);
+            this.ClientSize = new System.Drawing.Size(885, 587);
             this.Controls.Add(this.btnCapturar);
             this.Controls.Add(this.pctBoxCaptura);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmCapturaImagem";
             this.Text = "Captura de Imagem";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmCapturaImagem_FormClosing);
